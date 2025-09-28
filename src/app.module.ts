5641461f81213b4c9c76721909c4c5f806ad0b9e
daanberger <daan.berger@telenet.be>
2025-09-28 20:30:31 +0200

    Add Prisma module and fix typo in schema.prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { TeamsModule } from './teams/teams.module';
 import { SearchModule } from './search/search.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PokemonModule, TeamsModule, SearchModule]
+  imports: [PokemonModule, TeamsModule, SearchModule, PrismaModule]
 })
 export class AppModule {}
