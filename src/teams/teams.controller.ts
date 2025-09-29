3dbabf282866abd7734b6acb4edbbb8362c18488
daanberger <daan.berger@telenet.be>
2025-09-29 16:45:02 +0200

    Add team creation functionality

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -1,5 +1,6 @@
-import {Controller, Get, Param, Post} from "@nestjs/common";
+import {Body, Controller, Get, Param, Post} from "@nestjs/common";
 import {TeamService} from "./teams.service";
+import {CreateTeamDto} from "./dto/createTeam.dto";
 
 @Controller("api/v1")
 export class TeamsController {
@@ -12,8 +13,8 @@ export class TeamsController {
     }
 
     @Post("teams")
-    createTeam(){
-
+    createTeam(@Body() createTeamDto: CreateTeamDto){
+        return this.teamService.createTeam(createTeamDto);
     }
 
     @Get("teams/:id")
--- src/teams/teams.service.ts
+++ src/teams/teams.service.ts
@@ -1,20 +1,36 @@
 import {Injectable} from "@nestjs/common";
+import {PrismaService} from "../prisma/prisma.service";
+import {CreateTeamDto} from "./dto/createTeam.dto";
+import {TeamResponseDto} from "./dto/teamResponse.dto";
 
 @Injectable()
 export class TeamService {
-    getAllTeams() {
+    constructor(private prisma: PrismaService) {
+    }
+
+    async getAllTeams() {
 
     }
 
-    createTeam(){
+    async createTeam(createTeamDto: CreateTeamDto): Promise<TeamResponseDto> {
+        const team = await this.prisma.team.create({
+            data: {
+                name: createTeamDto.name,
+            }
+        });
+
+        return {
+            id: team.id,
+            name: team.name,
+            pokemons: []
+        };
+    }
+
+   async getTeamById(id: number) {
 
     }
 
-    getTeamById(id: number) {
-
-    }
-
-    setPokemonsForTeam(id: number) {
+    async setPokemonsForTeam(id: number) {
 
     }
 }
